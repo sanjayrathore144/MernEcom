070e40b842eb0b4b86aacc1d8903955fa462bb87
sanjay singh <sanjay.singh56@wipro.com>
2021-10-29 21:54:33 +0530

    updated by sanjay

---
+++ backend/routes/cart-routes.js
@@ -0,0 +1,54 @@
+var Router = require('express').Router();
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var ObjectId = require('mongodb').ObjectId;
+var MongoClient = require('mongodb').MongoClient;
+
+var app = express();
+
+app.use(cors());
+
+var Client = new MongoClient('mongodb+srv://mernecom:mernecom@cluster0.wtjik.mongodb.net/mernecom?retryWrites=true&w=majority');
+
+var connection; 
+
+Client.connect((err,db)=>{
+    if(!err)
+    {
+        connection=db;
+        console.log("Database connected sucessfully");
+    }
+    else
+    {
+        console.log("Database could not connect successfully");
+    }
+})
+
+Router.get('/',(req,res)=>{
+    var productdatabase = connection.db('mern_ecom').collection('user');
+    productdatabase.find().toArray((err,docs)=>{
+        if(!err)
+        {
+            res.send({status:"ok",data:docs});
+        }
+        else
+        {
+            res.send({status:"failed",data:err});
+        }
+    })
+})
+
+
+Router.post('/add-product',(req,res)=>{
+    var userCollection = connection.db('mern_ecom').collection('user');
+    userCollection.update({_id:ObjectId(req.body.user_id)}, { $push: { cart: req.body.product}},(err,res)=>{
+        
+    });
+
+})
+
+
+
+
+module.exports = Router;
